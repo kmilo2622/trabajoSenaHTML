c42de6e48242640bbbc3d8ef7d1afcaa098c699d
Juan Camilo Arroyave Rico <desarrolloweb@dynamicweb.co>
2017-06-16 11:56:29 -0500

    Función para explotar un string

--- js/angular.js
+++ js/angular.js
@@ -1,9 +1,42 @@
 var app = angular.module('app', []);
 
-app.controller('controller', function($scope) {
+app.filter('splitStringAndGetTheIndex', function() {
+	return function(input, splitChar, splitIndex) {
+		var inputChar = input.toString();
+		return inputChar.split(splitChar)[splitIndex];
+	}
+});
+
+app.filter('splitString', function() {
+	return function(input, splitChar) {
+		var inputChar = input.toString();
+		return inputChar.split(splitChar);
+	}
+});
+
+//Hagamos en esta parte todo el JavaScript sin Angular
+
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+	var scrolled = $(window).scrollTop();
+	$('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+	parallax();
+});
+
+//Y aquí comencemos toda la parte del angular
+
+app.controller('controllerHeader', function($scope) {
+	$scope.page_title = "Actividad de Aprendizaje";
+	$scope.nombre_estudiante = "Juan Camilo Arroyave Rico";
+	$scope.lenguajes = "HTML, CSS, JavaScript";
+});
+
+app.controller('controllerBody', function($scope) {
 	var titulo = $('#hiden_input').val();
 	$scope.name = titulo;
-	$scope.page_title = "Actividad de Aprendizaje";
 });
 
 app.controller('alertController', function($scope) {
@@ -25,4 +58,14 @@ app.controller('alertController', function($scope) {
 app.controller('valores', function($scope) {
 	$scope.valor = "A que no te imaginas en donde estoy...";
 	$('#hiden_input').val($scope.valor);
+});
+
+app.controller('navigationController', function($scope) {
+	$scope.LinkFunc = function(link) {
+		window.location.assign(link);
+	}
+});
+
+app.controller('footerController', function($scope) {
+	$scope.routa = location;
 });
